16e7b7f4461f5bd4f790766c4c73b492ae34d0b9
Sveta Piven <svetapiven93@icloud.com>
2022-09-21 09:57:36 +0300

    feat: new mess Freedom and peace

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("All dogs will come to heaven");
         System.out.println("Enjoy every moment ");
+        System.out.println("Freedom and peace");
     }
 }
