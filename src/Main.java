6034004b5c8930ff0c3b1ce1586c260d4d1863c9
Aleksandr Makovskiy <shish.kin199@gmail.com>
2022-09-21 10:06:37 +0300

    feat: Added mess to Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("All dogs will come to heaven");
         System.out.println("Enjoy every moment ");
         System.out.println("Freedom and peace");
+        System.out.println("Love save the world!");
     }
 }
