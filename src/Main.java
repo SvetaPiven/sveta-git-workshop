0d00178e6495f28cb1a9ddd0ac168a75db71ae1f
Sveta Piven <svetapiven93@icloud.com>
2022-09-21 09:33:33 +0300

    feat: add new mess in JavaClass Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("All dogs will come to heaven");
+        System.out.println("Enjoy every moment ");
     }
 }
